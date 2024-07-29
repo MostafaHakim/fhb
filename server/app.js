5785c883eac9f35e59d128915e19ea5e77de0b96
MostafaHakim <fhb.ashkona@gmail.com>
2024-07-29 16:18:36 +0600

    versel

--- server/app.js
+++ server/app.js
@@ -12,7 +12,11 @@ const creaditOrDebitRoute = require('./routes/creditOrDebitRoutes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:'locallhost/5000',
+    methods:'GET,POST,PUT,DELETE',
+    allowedHeaders:'Content-Type,Authorization'
+}))
 
 app.use('/teacher', teacherRoute)
 app.use('/salary', salaryRoute)
