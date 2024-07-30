96e6ccf6bff01e8bedf597cdf8567127fcb670be
MostafaHakim <fhb.ashkona@gmail.com>
2024-07-30 10:18:58 +0600

    update

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ db.on('error',console.error.bind(console, 'MongoDB connection error:'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-    origin:'locallhost/5000',
+    origin:'*',
     methods:'GET,POST,PUT,DELETE',
     allowedHeaders:'Content-Type,Authorization'
 }))
