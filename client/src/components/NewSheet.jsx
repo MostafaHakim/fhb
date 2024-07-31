2b38a0b9f371b0b1efdf0d14df07e7a6120cf533
MostafaHakim <89860668+MostafaHakim@users.noreply.github.com>
2024-08-01 00:39:33 +0600

    Update NewSheet.jsx

--- client/src/components/NewSheet.jsx
+++ client/src/components/NewSheet.jsx
@@ -8,11 +8,14 @@ import { useState } from "react";
 
 const NewSheet = () => {
     const [isLoading,setIsLoading]=useState(false)
+    setTimeOut(()=>{
+        setIsLoading(true)
+    },5000)
     const date = new Date()
     return (
         <>
             {
-                isLoading ? <div className="w-full flex flex-col items-center justify-center bg-white px-2">
+               !isLoading ? <div className="w-full flex flex-col items-center justify-center bg-white px-2">
                 <div className="w-full grid grid-cols-3 gap-4">
                     <div className="col-span-1 w-full">
                       <DailyTotalCollectionHistory />
