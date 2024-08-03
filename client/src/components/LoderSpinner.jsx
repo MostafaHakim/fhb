35d7f016681cd8e6fea0f916f48a039fa09db7fb
FHB <fhb.ashkona@gmail.com>
2024-08-03 14:15:20 +0600

    new style

--- client/src/components/LoderSpinner.jsx
+++ client/src/components/LoderSpinner.jsx
@@ -3,7 +3,7 @@ import DotLoader from 'react-spinners/DotLoader'
 
 function LoderSpinner() {
   return (
-    <div className='w-full absolute top-0 left-0 flex flex-col items-center justify-center'>
+    <div className='w-full absolute mt-40 left-0 flex flex-col items-center justify-center'>
         <DotLoader color="#67f2d1" />
     </div>
   )
