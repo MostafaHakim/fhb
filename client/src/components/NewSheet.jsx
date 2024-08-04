0ed0d8e39e95f7abcdc75b01053a76f620d9abe4
FHB <fhb.ashkona@gmail.com>
2024-08-04 19:00:55 +0600

    new Commit

--- client/src/components/NewSheet.jsx
+++ client/src/components/NewSheet.jsx
@@ -42,7 +42,7 @@ const NewSheet = () => {
     return (
         <>
         <div className="w-full flex flex-col items-center justify-center bg-white px-2 relative">
-            { isLoading && <LoderSpinner /> }
+            { !isLoading && <LoderSpinner /> }
             <div className="w-full grid grid-cols-3 gap-4">
                 <div className="col-span-1 w-full">
                   <DailyTotalCollectionHistory />
