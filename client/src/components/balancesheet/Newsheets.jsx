90ab60867bdff48b328b2ef22b9e28cacac20092
FHB <fhb.ashkona@gmail.com>
2024-08-19 18:40:18 +0600

    nz

--- client/src/components/balancesheet/Newsheets.jsx
+++ client/src/components/balancesheet/Newsheets.jsx
@@ -57,34 +57,58 @@ const ItemForm = () => {
     return (
         <form onSubmit={handleSubmit}>
             {loading && <LoaderSpinner />}
-            <div className='w-full grid grid-cols-3 gap-1'>
-                <div className='col-span-1 w-full'>
-                    {items.filter(item => {
-                        if (item.optionType == "Credit") {
-                            return item
-                        }
-                    }).map((item, i) => (
-                        <div className='w-full grid grid-cols-7 border-[1px] border-t-0 border-slate-700 m-auto' key={item._id} >
-                            <label className='col-span-1 w-full text-center border-r-[1px] border-slate-700 '>{i + 1}</label>
-                            <label className='col-span-2 w-full   border-r-[1px] border-slate-700 text-start px-4'>{item.optionName}</label>
-                            <input type="number" name={`amount-${item._id}`} placeholder="Amount" className='col-span-2 w-full text-center  border-r-[1px] border-slate-700' />
-                            <input type="number" name={`quantity-${item._id}`} placeholder="Quantity" className='col-span-2 w-full text-center' />
+            <div className='w-full p-10 flex flex-col items-center justify-center bg-white'>
+                <div></div>
+                <div>
+                    <div className='w-full grid grid-cols-2 gap-1'>
+                        <div className='col-span-1 w-full  border-t-[1px] border-slate-700'>
+                            {items.filter(item => {
+                                if (item.optionType == "Credit") {
+                                    return item
+                                }
+                            }).map((item, i) => (
+                                <div className='w-full grid grid-cols-7 border-[1px] border-t-0 border-slate-700 m-auto' key={item._id} >
+                                    <label className='col-span-1 w-full text-center border-r-[1px] border-slate-700 '>{i + 1}</label>
+                                    <label className='col-span-2 w-full   border-r-[1px] border-slate-700 text-start px-4'>{item.optionName}</label>
+                                    <input type="number" name={`quantity-${item._id}`} placeholder="Quantity" className='col-span-2 w-full text-center border-r-[1px] border-slate-700' />
+                                    <input type="number" name={`amount-${item._id}`} placeholder="Amount" className='col-span-2 w-full text-center' />
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                        <div className='col-span-1 w-full border-t-[1px] border-slate-700'>
+                            {items.filter(item => {
+                                if (item.optionType != "Credit"&&item.optionType != "IOU") {
+                                    return item
+                                }
+                            }).map((item, i) => (
+                                <div className='w-full grid grid-cols-7 border-[1px] border-t-0 border-slate-700 m-auto' key={item._id} >
+                                    <label className='col-span-1 w-full text-center border-r-[1px] border-slate-700 '>{i + 1}</label>
+                                    <label className='col-span-2 w-full   border-r-[1px] border-slate-700 text-start px-4'>{item.optionName}</label>
+                                    <input type="number" name={`quantity-${item._id}`} placeholder="Quantity" className='col-span-2 w-full text-center border-r-[1px] border-slate-700' />
+                                    <input type="number" name={`amount-${item._id}`} placeholder="Amount" className='col-span-2 w-full text-center' />
+                                </div>
+                            ))}
+                            <div className='w-full grid grid-cols-7 border-[1px] border-t-0 border-slate-700 m-auto bg-green-200' >
+                                    <label className='col-span-5 w-full text-center border-r-[1px] border-slate-700 '>Total</label>
+                                    <label className='col-span-2 w-full text-center  border-r-[1px] border-slate-700  px-4'>5000</label>
+                            </div>
+                            <div className='w-full grid grid-cols-1 border-[1px] border-t-0 border-slate-700 m-auto bg-green-200' >
+                                    <label className='col-span-1 w-full text-center border-r-[1px] border-slate-700 '>IOU</label>
+                            </div>
+                            {items.filter(item => {
+                                if (item.optionType != "Credit" && item.optionType != "Debit") {
+                                    return item
+                                }
+                            }).map((item, i) => (
+                                <div className='w-full grid grid-cols-7 border-[1px] border-t-0 border-slate-700 m-auto' key={item._id} >
+                                    <label className='col-span-1 w-full text-center border-r-[1px] border-slate-700 '>{i + 1}</label>
+                                    <label className='col-span-2 w-full   border-r-[1px] border-slate-700 text-start px-4'>{item.optionName}</label>
+                                    <input type="number" name={`quantity-${item._id}`} placeholder="Quantity" className='col-span-2 w-full text-center border-r-[1px] border-slate-700' />
+                                    <input type="number" name={`amount-${item._id}`} placeholder="Amount" className='col-span-2 w-full text-center ' />
+                                </div>
+                            ))}
+                        </div>
                 </div>
-                <div className='col-span-1 w-full'>
-                    {items.filter(item => {
-                        if (item.optionType != "Credit") {
-                            return item
-                        }
-                    }).map((item, i) => (
-                        <div className='w-full grid grid-cols-7 border-[1px] border-t-0 border-slate-700 m-auto' key={item._id} >
-                            <label className='col-span-1 w-full text-center border-r-[1px] border-slate-700 '>{i + 1}</label>
-                            <label className='col-span-2 w-full   border-r-[1px] border-slate-700 text-start px-4'>{item.optionName}</label>
-                            <input type="number" name={`amount-${item._id}`} placeholder="Amount" className='col-span-2 w-full text-center  border-r-[1px] border-slate-700' />
-                            <input type="number" name={`quantity-${item._id}`} placeholder="Quantity" className='col-span-2 w-full text-center' />
-                        </div>
-                    ))}
                 </div>
             </div>
             
